add loss normalization to crfs - something is wrong with frank-wolfe now :-/

examples/plot_svm_objectives.py
@@ -18,7 +18,7 @@ from pystruct.learners import (NSlackSSVM, OneSlackSSVM, SubgradientSSVM,
                                FrankWolfeSSVM)
 from pystruct.datasets import generate_crosses_explicit
 
-X, Y = generate_crosses_explicit(n_samples=50, noise=10, size=6, n_crosses=1)
+X, Y = generate_crosses_explicit(n_samples=250, noise=10, size=6, n_crosses=1)
 n_labels = len(np.unique(Y))
 crf = GridCRF(n_states=n_labels, inference_method="dai")
 
@@ -28,7 +28,7 @@ one_slack_svm = OneSlackSSVM(crf, check_constraints=False,
                              max_iter=100, tol=0.001, inference_cache=50)
 subgradient_svm = SubgradientSSVM(crf, learning_rate=0.001, max_iter=20,
                                   decay_exponent=0, momentum=0)
-bcfw_svm = FrankWolfeSSVM(crf, max_iter=50, verbose=2, check_dual_every=4)
+bcfw_svm = FrankWolfeSSVM(crf, max_iter=150, verbose=2, check_dual_every=4)
 
 #n-slack cutting plane ssvm
 n_slack_svm.fit(X, Y)
@@ -47,23 +47,41 @@ inference_run = ~np.array(one_slack_svm.cached_constraint_)
 time_one = np.array(one_slack_svm.timestamps_[1:])[inference_run]
 
 # plot stuff
-plt.plot(n_slack_svm.timestamps_[1:], n_slack_svm.objective_curve_,
-         label="n-slack cutting plane")
-plt.plot(n_slack_svm.timestamps_[1:], n_slack_svm.primal_objective_curve_,
-         label="n-slack primal")
-plt.plot(time_one,
-         np.array(one_slack_svm.objective_curve_)[inference_run],
-         label="one-slack cutting_plane")
-plt.plot(time_one,
-         np.array(one_slack_svm.primal_objective_curve_)[inference_run],
-         label="one-slack primal")
-plt.plot(subgradient_svm.timestamps_[1:], subgradient_svm.objective_curve_,
-         label="subgradient")
-plt.plot(bcfw_svm.timestamps_[1:], bcfw_svm.objective_curve_,
-         label="Block-Coordinate Frank-Wolfe Dual")
-plt.plot(bcfw_svm.timestamps_[1:], bcfw_svm.primal_objective_curve_,
-         label="Block-Coordinate Frank-Wolfe Primal")
-plt.legend()
-plt.yscale('log')
-plt.xlabel("training time")
+fig, ax = plt.subplots(2)
+ax[0].plot(n_slack_svm.timestamps_[1:], n_slack_svm.objective_curve_,
+           label="n-slack cutting plane")
+ax[0].plot(n_slack_svm.timestamps_[1:], n_slack_svm.primal_objective_curve_,
+           label="n-slack primal")
+ax[0].plot(time_one, np.array(one_slack_svm.objective_curve_)[inference_run],
+           label="one-slack cutting_plane")
+ax[0].plot(time_one,
+           np.array(one_slack_svm.primal_objective_curve_)[inference_run],
+           label="one-slack primal")
+ax[0].plot(subgradient_svm.timestamps_[1:], subgradient_svm.objective_curve_,
+           label="subgradient")
+ax[0].plot(bcfw_svm.timestamps_[1:], bcfw_svm.objective_curve_,
+           label="Block-Coordinate Frank-Wolfe Dual")
+ax[0].plot(bcfw_svm.timestamps_[1:], bcfw_svm.primal_objective_curve_,
+           label="Block-Coordinate Frank-Wolfe Primal")
+ax[0].legend()
+ax[0].set_xlabel("training time")
+
+ax[1].plot(n_slack_svm.objective_curve_, label="n-slack cutting plane")
+ax[1].plot(n_slack_svm.primal_objective_curve_, label="n-slack primal")
+ax[1].plot(np.where(inference_run)[0],
+           np.array(one_slack_svm.objective_curve_)[inference_run],
+           label="one-slack cutting_plane")
+ax[1].plot(np.where(inference_run)[0],
+           np.array(one_slack_svm.primal_objective_curve_)[inference_run],
+           label="one-slack primal")
+ax[1].plot(subgradient_svm.objective_curve_, label="subgradient")
+bcfc_iterations = np.arange(len(bcfw_svm.objective_curve_)) * bcfw_svm.check_dual_every
+ax[1].plot(bcfc_iterations, bcfw_svm.objective_curve_,
+           label="Block-Coordinate Frank-Wolfe Dual")
+ax[1].plot(bcfc_iterations, bcfw_svm.primal_objective_curve_,
+           label="Block-Coordinate Frank-Wolfe Primal")
+ax[1].legend()
+ax[1].set_xlabel("iterations")
 plt.show()
+from IPython.core.debugger import Tracer
+Tracer()()

pystruct/models/base.py
@@ -58,9 +58,12 @@ class StructuredModel(object):
         # hamming loss:
         if isinstance(y_hat, tuple):
             return self.continuous_loss(y, y_hat[0])
+        losses = y != y_hat
         if hasattr(self, 'class_weight'):
-            return np.sum(self.class_weight[y] * (y != y_hat))
-        return np.sum(y != y_hat)
+            losses = self.class_weight[y] * losses
+        if getattr(self, 'normalize_loss', False):
+            return np.mean(losses)
+        return np.sum(losses)
 
     def batch_loss(self, Y, Y_hat):
         # default implementation of batch loss
@@ -82,7 +85,9 @@ class StructuredModel(object):
         # all entries minus correct ones
         result = 1 - y_hat[gx, y]
         if hasattr(self, 'class_weight'):
-            return np.sum(self.class_weight[y] * result)
+            result = self.class_weight[y] * result
+        if getattr(self, 'normalize_loss', False):
+            return np.mean(result)
         return np.sum(result)
 
     def loss_augmented_inference(self, x, y, w, relaxed=None):

pystruct/models/crf.py
@@ -8,7 +8,7 @@ from .utils import loss_augment_unaries
 class CRF(StructuredModel):
     """Abstract base class"""
     def __init__(self, n_states=None, n_features=None, inference_method=None,
-                 class_weight=None):
+                 class_weight=None, normalize_loss=True):
         self.n_states = n_states
         if inference_method is None:
             # get first in list that is installed
@@ -17,6 +17,7 @@ class CRF(StructuredModel):
         self.inference_calls = 0
         self.n_features = n_features
         self.class_weight = class_weight
+        self.normalize_loss = normalize_loss
         self._set_size_psi()
         self._set_class_weight()
 
@@ -103,7 +104,8 @@ class CRF(StructuredModel):
         unary_potentials = self._get_unary_potentials(x, w)
         pairwise_potentials = self._get_pairwise_potentials(x, w)
         edges = self._get_edges(x)
-        loss_augment_unaries(unary_potentials, np.asarray(y), self.class_weight)
+        y = np.asarray(y)
+        loss_augment_unaries(unary_potentials, np.asarray(y), self.class_weight / len(y))
 
         return inference_dispatch(unary_potentials, pairwise_potentials, edges,
                                   self.inference_method, relaxed=relaxed,