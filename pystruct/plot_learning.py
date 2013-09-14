3e5933ffd394000d280cefdfbbd8e63f22a6aaf0
Andy <amueller@ais.uni-bonn.de>
2013-09-14 18:32:36 +0200

    even better plotting script with argparse.

--- pystruct/plot_learning.py
+++ pystruct/plot_learning.py
@@ -2,8 +2,7 @@
 """
 This module provides a callable for easy evaluation of stored models.
 """
-import sys
-
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -11,9 +10,18 @@ from pystruct.utils import SaveLogger
 
 
 def main():
-    argv = sys.argv
+
+    parser = argparse.ArgumentParser(description='Plot learning progress for one or several SSVMs.')
+    parser.add_argument('pickles', metavar='N', type=str, nargs='+',
+                        help='pickle files containing SSVMs')
+    parser.add_argument('--time', dest='time', action='store_const',
+                        const=True, default=False, help='Plot against '
+                       'wall-clock time (default: plot against iterations.)')
+
+    args = parser.parse_args()
+
     ssvms = []
-    for file_name in argv[1:]:
+    for file_name in args.pickles:
         print("loading %s ..." % file_name)
         ssvms.append(SaveLogger(file_name=file_name).load())
     if np.any([hasattr(ssvm, 'loss_curve_') for ssvm in ssvms]):
@@ -21,11 +29,11 @@ def main():
     else:
         n_plots = 1
     fig, axes = plt.subplots(1, n_plots)
-    for ssvm, file_name in zip(ssvms, argv[1:]):
+    for ssvm, file_name in zip(ssvms, args.pickles):
         prefix = ""
         if len(ssvms) > 1:
             prefix = file_name[:-7] + " "
-        plot_learning(ssvm, axes=axes, prefix=prefix)
+        plot_learning(ssvm, axes=axes, prefix=prefix, time=args.time)
     plt.show()
 
 
@@ -41,7 +49,7 @@ def plot_learning(ssvm, time=True, axes=None, prefix=""):
     Parameters
     -----------
     ssvm : object
-        SSVM learner to evaluate. Should work with all learners.
+        Learner to evaluate. Should work with all learners.
 
     time : boolean, default=True
         Whether to use wall clock time instead of iterations as the x-axis.
@@ -59,17 +67,22 @@ def plot_learning(ssvm, time=True, axes=None, prefix=""):
     if hasattr(ssvm, 'base_ssvm'):
         ssvm = ssvm.base_ssvm
 
-    if hasattr(ssvm, 'dual_objective_curve_'):
+    if hasattr(ssvm, 'iterations_'):
+        # BCFW remembers when we computed the objective
+        iterations = ssvm.iterations_
+    elif hasattr(ssvm, 'dual_objective_curve_'):
         iterations = np.arange(len(ssvm.dual_objective_curve_))
         print("Dual Objective: %f" % ssvm.dual_objective_curve_[-1])
     else:
         iterations = np.arange(len(ssvm.primal_objective_curve_))
         print("Primal Objective: %f" % ssvm.primal_objective_curve_[-1])
 
-    print("Iterations: %d" % len(iterations))
+    print("Iterations: %d" % (np.max(iterations) + 1))  # we count from 0
     inference_run = None
     if hasattr(ssvm, 'cached_constraint_'):
-        inference_run = ~np.array(ssvm.cached_constraint_)
+        if np.any(ssvm.cached_constraint_):
+            # we don't want to do this if there was no constraint caching
+            inference_run = ~np.array(ssvm.cached_constraint_)
     if hasattr(ssvm, "loss_curve_"):
         n_plots = 2
     else:
@@ -79,19 +92,17 @@ def plot_learning(ssvm, time=True, axes=None, prefix=""):
     if not isinstance(axes, list):
         axes = [axes]
 
-    if time and hasattr(ssvm, 'timestamps_'):
-        print("loading timestamps")
+    if time:
         inds = np.array(ssvm.timestamps_)
         inds = inds[1:] / 60.
-        #inds = np.hstack([inds, [inds[-1]]])
         axes[0].set_xlabel('training time (min)')
     else:
-        axes[0].set_xlabel('QP iterations')
+        axes[0].set_xlabel('Passes through training data')
         inds = iterations
 
     axes[0].set_title("Objective")
     if hasattr(ssvm, "dual_objective_curve_"):
-        axes[0].plot(inds, ssvm.dual_objective_curve_, label=prefix + "dual objective")
+        axes[0].plot(inds, ssvm.dual_objective_curve_, '--', label=prefix + "dual objective")
         axes[0].set_yscale('log')
     if hasattr(ssvm, "primal_objective_curve_"):
         axes[0].plot(inds, ssvm.primal_objective_curve_,
@@ -102,12 +113,12 @@ def plot_learning(ssvm, time=True, axes=None, prefix=""):
         axes[0].plot(inds[inference_run],
                      np.array(ssvm.primal_objective_curve_)[inference_run],
                      'o', label=prefix + "primal")
-    axes[0].legend()
+    axes[0].legend(loc='best')
     if n_plots == 2:
-        if time and hasattr(ssvm, "timestamps_"):
+        if time:
             axes[1].set_xlabel('training time (min)')
         else:
-            axes[1].set_xlabel('QP iterations')
+            axes[1].set_xlabel('Passes through training data')
 
         try:
             axes[1].plot(inds[::ssvm.show_loss_every], ssvm.loss_curve_)
