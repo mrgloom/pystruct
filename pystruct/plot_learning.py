57f250f3018ac61b434a3d65df50efa3a29d8240
Andy <amueller@ais.uni-bonn.de>
2013-09-14 17:46:57 +0200

    slight cleanup in plot_learning, way more powerful now :) can plot multiple files at once.

--- pystruct/plot_learning.py
+++ pystruct/plot_learning.py
@@ -12,12 +12,24 @@ from pystruct.utils import SaveLogger
 
 def main():
     argv = sys.argv
-    print("loading %s ..." % argv[1])
-    ssvm = SaveLogger(file_name=argv[1]).load()
-    plot_learning(ssvm)
+    ssvms = []
+    for file_name in argv[1:]:
+        print("loading %s ..." % file_name)
+        ssvms.append(SaveLogger(file_name=file_name).load())
+    if np.any([hasattr(ssvm, 'loss_curve_') for ssvm in ssvms]):
+        n_plots = 2
+    else:
+        n_plots = 1
+    fig, axes = plt.subplots(1, n_plots)
+    for ssvm, file_name in zip(ssvms, argv[1:]):
+        prefix = ""
+        if len(ssvms) > 1:
+            prefix = file_name[:-7] + " "
+        plot_learning(ssvm, axes=axes, prefix=prefix)
+    plt.show()
 
 
-def plot_learning(ssvm, time=True):
+def plot_learning(ssvm, time=True, axes=None, prefix=""):
     """Plot optimization curves and cache hits.
 
     Create a plot summarizing the optimization / learning process of an SSVM.
@@ -34,6 +46,9 @@ def plot_learning(ssvm, time=True):
     time : boolean, default=True
         Whether to use wall clock time instead of iterations as the x-axis.
 
+    prefix : string, default=""
+        Prefix for legend.
+
     Notes
     -----
     Warm-starting a model might mess up the alignment of the curves.
@@ -43,43 +58,50 @@ def plot_learning(ssvm, time=True):
     print(ssvm)
     if hasattr(ssvm, 'base_ssvm'):
         ssvm = ssvm.base_ssvm
-    print("Iterations: %d" % len(ssvm.objective_curve_))
-    print("Objective: %f" % ssvm.objective_curve_[-1])
+
+    if hasattr(ssvm, 'dual_objective_curve_'):
+        iterations = np.arange(len(ssvm.dual_objective_curve_))
+        print("Dual Objective: %f" % ssvm.dual_objective_curve_[-1])
+    else:
+        iterations = np.arange(len(ssvm.primal_objective_curve_))
+        print("Primal Objective: %f" % ssvm.primal_objective_curve_[-1])
+
+    print("Iterations: %d" % len(iterations))
     inference_run = None
     if hasattr(ssvm, 'cached_constraint_'):
         inference_run = ~np.array(ssvm.cached_constraint_)
-        print("Gap: %f" %
-              (np.array(ssvm.primal_objective_curve_)[inference_run][-1] -
-               ssvm.objective_curve_[-1]))
     if hasattr(ssvm, "loss_curve_"):
         n_plots = 2
-        fig, axes = plt.subplots(1, 2)
     else:
         n_plots = 1
-        fig, axes = plt.subplots(1, 1)
+    if axes is None:
+        fig, axes = plt.subplots(1, n_plots)
+    if not isinstance(axes, list):
         axes = [axes]
+
     if time and hasattr(ssvm, 'timestamps_'):
         print("loading timestamps")
         inds = np.array(ssvm.timestamps_)
-        inds = inds[2:len(ssvm.objective_curve_) + 1] / 60.
-        inds = np.hstack([inds, [inds[-1]]])
+        inds = inds[1:] / 60.
+        #inds = np.hstack([inds, [inds[-1]]])
         axes[0].set_xlabel('training time (min)')
     else:
-        inds = np.arange(len(ssvm.objective_curve_))
         axes[0].set_xlabel('QP iterations')
+        inds = iterations
 
     axes[0].set_title("Objective")
-    axes[0].plot(inds, ssvm.objective_curve_, label="dual")
-    axes[0].set_yscale('log')
+    if hasattr(ssvm, "dual_objective_curve_"):
+        axes[0].plot(inds, ssvm.dual_objective_curve_, label=prefix + "dual objective")
+        axes[0].set_yscale('log')
     if hasattr(ssvm, "primal_objective_curve_"):
         axes[0].plot(inds, ssvm.primal_objective_curve_,
-                     label="cached primal" if inference_run is not None
-                     else "primal")
+                     label=prefix + "cached primal objective" if inference_run is not None
+                     else prefix + "primal objective")
     if inference_run is not None:
-        inference_run = inference_run[:len(ssvm.objective_curve_)]
+        inference_run = inference_run[:len(ssvm.dual_objective_curve_)]
         axes[0].plot(inds[inference_run],
                      np.array(ssvm.primal_objective_curve_)[inference_run],
-                     'o', label="primal")
+                     'o', label=prefix + "primal")
     axes[0].legend()
     if n_plots == 2:
         if time and hasattr(ssvm, "timestamps_"):
@@ -94,7 +116,7 @@ def plot_learning(ssvm, time=True):
 
         axes[1].set_title("Training Error")
         axes[1].set_yscale('log')
-    plt.show()
+    return axes
 
 
 if __name__ == "__main__":
